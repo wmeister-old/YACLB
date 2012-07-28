064bf8d1169af0e7d1abe0a3d5e63f3a304a4710
William Meister <wmeister86@gmail.com>
2012-07-28 09:25:07 -0500

    downcase commands when checking for a match; use keywords instead of strings in defcmd

--- macro.lisp
+++ macro.lisp
@@ -2,8 +2,8 @@
 
 (defmacro defcmd (trigger func)
   `(push #'(lambda (message)
-	    (when (equal (concatenate 'string "." ,trigger)
-			 (car (split-on-spaces (second (irc:arguments message)))))
+	    (when (equal (concatenate 'string "." (string-downcase (string ,trigger)))
+			 (string-downcase (car (split-on-spaces (second (irc:arguments message))))))
 	      (let* ((args (cdr (split-on-spaces (second (irc:arguments message)))))
 		     (num-args (length args))
 		     (sender (irc:source message)))
