6178bb56cc5ac992040cf5f9209cb17af2ebb106
William Meister <wmeister86@gmail.com>
2012-07-28 08:21:54 -0500

    added uptime command

--- command.lisp
+++ command.lisp
@@ -3,6 +3,7 @@
 (defcmd "args" (reply (format nil "~D args = ~A" num-args args)))
 (defcmd "sender" (reply (format nil "~A" (irc:source message))))
 (defcmd "uname" (reply (inferior-shell:run/ss "uname -a")))
+(defcmd "uptime" (reply (inferior-shell:run/ss "uptime")))
 (defcmd "dbver" (reply (format nil "CouchDB v~A" (clouchdb:document-property :|version| (clouchdb:get-couchdb-info)))))
 (defcmd "dbinfo"
     (let ((info (clouchdb:get-db-info)))
